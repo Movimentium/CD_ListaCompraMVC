0beb418f477c685a6cfd7695d5200394b31865b6
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2020-06-10 02:02:43 +0200

    Initial Commit

---
+++ CD_ListaCompraMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CD_ListaCompraMVC
+//
+//  Created by Miguel on 10/06/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
